934abd47bad85bca44f89b239139395261048b13
Fabien <fvgendt@gmail.com>
2021-05-16 14:22:47 +0200

    stripes

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,6 +87,7 @@ jQuery(function ($) {
 		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			console.log(target)
 			if (target.length) {
 				html_body.animate({
 					scrollTop: target.offset().top - 50
