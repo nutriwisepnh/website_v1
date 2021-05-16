3973e87203eaffbc1ceadd6af900b7792495bf9d
Fabien <fvgendt@gmail.com>
2021-05-16 14:29:53 +0200

    stripes

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,6 +84,7 @@ jQuery(function ($) {
 
 	var html_body = $('html, body');
 	$('nav a, .page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+		console.log(location)
 		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
